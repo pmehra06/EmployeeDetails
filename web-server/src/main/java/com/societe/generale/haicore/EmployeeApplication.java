b0a10e3dec03543713caf81f10fd29bc3afb505f
Pooja mehra <poojamehra911@gmail.com>
2021-07-06 15:31:08 +0530

    Modify classes

--- web-server/src/main/java/com/societe/generale/haicore/EmployeeApplication.java
+++ web-server/src/main/java/com/societe/generale/haicore/EmployeeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HaiAppApplication {
+public class EmployeeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HaiAppApplication.class, args);
+		SpringApplication.run(EmployeeApplication.class, args);
 	}
 
 }
--- web-server/src/main/java/com/societe/generale/haicore/ServletInitializer.java
+++ web-server/src/main/java/com/societe/generale/haicore/ServletInitializer.java
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(HaiAppApplication.class);
+		return application.sources(EmployeeApplication.class);
 	}
 
 }
